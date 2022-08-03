0645f4a784b4eb0e0d91f922bd101705a01e9287
YuseiAbe <yabe@diverta.co.jp>
2022-08-03 16:46:40 +0900

    fix: sitekey condition

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 export default function ({ $axios, route }) {
     const sitekey = localStorage.getItem('sitekey');
-    if (sitekey !== undefined && sitekey !== null && sitekey !== 'undefined' && sitekey !== 'null' && sitekey.match(/^[A-Za-z0-9-]{3,30}$/)) {
+    if (sitekey !== undefined && sitekey !== null && sitekey !== 'undefined' && sitekey !== 'null' && sitekey.match(/^[A-Za-z0-9-]{3,}$/)) {
         $axios.defaults.baseURL = 'https://' + sitekey + '.g.kuroco.app';
         if (sitekey === 'dev-nuxt-auth') { $axios.defaults.baseURL = 'https://dev-nuxt-auth.a.kuroco.app'; }
     }
