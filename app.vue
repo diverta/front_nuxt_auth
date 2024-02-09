a00f8c382cb74bd1adecc0d13219ea4c5e938455
YuseiAbe <yabe@diverta.co.jp>
2024-02-09 11:41:56 +0900

    feat: restore user profile for first access

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
-
-import { NuxtLayout } from '#build/components';
 <template>
   <NuxtLayout>
-  <div>
-    <NuxtPage/>
-    <!-- <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1> -->
-  </div>
-</NuxtLayout>
+    <div>
+      <NuxtPage />
+      <!-- <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1> -->
+    </div>
+  </NuxtLayout>
 </template>
+
+<script setup>
+await useAuth().profile();
+</script>
