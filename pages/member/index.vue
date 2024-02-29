169a378b1245d95122af9bc5b22a763d557620b0
VortexExpansion <gaurav@diverta.co.jp>
2024-02-29 18:28:59 +0900

    implement member list page

---
+++ pages/member/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div v-if="done">
+    <template v-if="isDisallowed">
+      <v-card>
+        <v-card-text class="text-center">
+          {{ $t("member.disallowed") }}
+        </v-card-text>
+      </v-card>
+    </template>
+    <template v-else>
+      <v-row>
+        <v-col class="col-sm-6 col-12 py-0">
+          <v-autocomplete
+            v-model="currName"
+            :items="names"
+            dense
+            filled
+            :label="$t('member.search_member')"
+          />
+        </v-col>
+        <v-col class="col-sm-6 col-12 py-0">
+          <v-autocomplete
+            v-model="currDepartment"
+            :items="departments"
+            dense
+            filled
+            :label="$t('member.department')"
+          />
+        </v-col>
+      </v-row>
+      <v-data-table
+        :headers="headers"
+        :items="userList"
+        class="elevation-1 mt-5 c-table"
+      >
+        <template v-slot:item.name="{ item, value }">
+          <NuxtLink :to="`/member/detail/${item.id}`" no-prefetch>
+            {{ value }}
+          </NuxtLink>
+        </template>
+        <template v-slot:item.phone="{ value }">
+          <a :href="`tel:${value}`">{{ value }}</a>
+        </template>
+      </v-data-table>
+    </template>
+  </div>
+</template>
+<script setup>
+const { authUser } = useAuth();
+const userList = ref([]);
+const currName = ref("");
+const currDepartment = ref("");
+const done = ref(false);
+const isDisallowed = ref(false);
+const perpage = ref(10);
+const page = ref(1);
+const pageCount = ref(0);
+
+const headers = [
+  { title: "Name", value: "name", sortable: false },
+  { title: "Deparment", value: "department", sortable: false },
+  { title: "Position", value: "position", sortable: false },
+  { title: "Phone", value: "phone", sortable: false },
+];
+
+const names = computed(() => {
+  return ["", ...userList.value.map(({ name }) => name)];
+});
+
+const departments = computed(() => {
+  return [
+    "",
+    ...userList.value.map(({ department }) => department).filter((d) => d),
+  ];
+});
+
+const filteredItems = computed(() => {
+  return userList.value
+    .filter(({ department }) =>
+      currDepartment === "" ? true : department === currDepartment
+    )
+    .filter(({ name }) => (currName === "" ? true : name === currName));
+});
+
+onMounted(async () => {
+  try {
+    const { list } = await $fetch(
+      `${apiDomain.baseURL}/rcms-api/1/member/list`,
+      {
+        credentials: "include",
+        server: false,
+      }
+    );
+    console.log(list);
+    userList.value = list.map((item) => ({
+      name: `${item.name1} ${item.name2}`,
+      department: item.department,
+      position: item.position,
+      phone: item.tel,
+      id: item.member_id,
+    }));
+    console.log(userList.value);
+  } catch (error) {
+    console.error(error);
+    if (error?.response?.status) {
+      isDisallowed = true;
+    }
+  }
+  done.value = true;
+});
+</script>
