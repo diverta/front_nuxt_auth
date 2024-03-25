5519e3bce94c8faeb01cb85c747e468561b49f9b
VortexExpansion <gaurav@diverta.co.jp>
2024-03-25 19:47:32 +0900

    wip reminder

---
+++ pages/reminder.vue
@@ -0,0 +1,17 @@
+<template></template>
+<script setup>
+const token = ref('');
+const e1 = ref('');
+const valid = ref(true);
+const email = ref('');
+const password_show = ref(false);
+const login_pwd = ref('');
+const password_show2 = ref(false);
+const login_pwd2 = ref('');
+const temp_pwd = ref('');
+const loading1 = ref(false);
+const loading2 = ref(false);
+
+
+
+</script>
