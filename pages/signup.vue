472cfd6f570154fc335c6fca636242469578645a
YuseiAbe <yabe@diverta.co.jp>
2024-03-30 13:35:50 +0900

    fix: lint,format

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,38 +1,25 @@
 <template>
-  <ClientOnly>
-    <div class="l-content_maxWidth-sm mb-5 pb-5">
-      <v-progress-linear
-        :active="loading"
-        :indeterminate="loading"
-        absolute
-        top
-        color="orange white-4"
-      />
-      <div class="v-stepper c-form_wrap bg-white">
-        <v-container fluid>
-          <div class="l-content_heading">
-            <h1>{{ $t('signup.title') }}</h1>
-            <h4 class="slogan">
-              {{ $t('signup.text') }}
-            </h4>
-          </div>
+    <ClientOnly>
+        <div class="l-content_maxWidth-sm mb-5 pb-5">
+            <v-progress-linear :active="loading" :indeterminate="loading" absolute top color="orange white-4" />
+            <div class="v-stepper c-form_wrap bg-white">
+                <v-container fluid>
+                    <div class="l-content_heading">
+                        <h1>{{ $t('signup.title') }}</h1>
+                        <h4 class="slogan">
+                            {{ $t('signup.text') }}
+                        </h4>
+                    </div>
 
-          <ProfileForm @submit="handleSubmit">
-            <template v-slot:headInput>
-              <FormKit
-                v-model="sitekey"
-                name="sitekey"
-                :label="$t('login.site_key')"
-                type="text"
-                :classes="{ outer: 'signup-form_elm-sitekey' }"
-                validation="required|length:0,100"
-              />
-            </template>
-          </ProfileForm>
-        </v-container>
-      </div>
-    </div>
-  </ClientOnly>
+                    <ProfileForm @submit="handleSubmit">
+                        <template #headInput>
+                            <FormKit v-model="sitekey" name="sitekey" :label="$t('login.site_key')" type="text" :classes="{ outer: 'signup-form_elm-sitekey' }" validation="required|length:0,100" />
+                        </template>
+                    </ProfileForm>
+                </v-container>
+            </div>
+        </div>
+    </ClientOnly>
 </template>
 <script setup>
 const snackbar = useSnackbar();
@@ -41,41 +28,38 @@ const loading = ref(false);
 const sitekey = ref(apiDomain.sitekey);
 
 const handleSubmit = async (formValues) => {
-  loading.value = true;
+    loading.value = true;
 
-  apiDomain.sitekey = sitekey.value;
-  apiDomain.baseURL =
-    apiDomain.sitekey === 'dev-nuxt-auth'
-      ? 'https://dev-nuxt-auth.a.kuroco.app'
-      : `https://${apiDomain.sitekey}.g.kuroco.app`;
+    apiDomain.sitekey = sitekey.value;
+    apiDomain.baseURL = apiDomain.sitekey === 'dev-nuxt-auth' ? 'https://dev-nuxt-auth.a.kuroco.app' : `https://${apiDomain.sitekey}.g.kuroco.app`;
 
-  try {
-    await register({
-      ...formValues,
-      // remove unnecessary fields
-      sitekey: undefined,
-      term: undefined,
-    });
-  } catch (error) {
-    snackbar.add({
-      type: 'error',
-      text: error?.response?._data?.errors?.[0]?.message || 'An error occurred',
-    });
-  } finally {
-    loading.value = false;
-  }
+    try {
+        await register({
+            ...formValues,
+            // remove unnecessary fields
+            sitekey: undefined,
+            term: undefined
+        });
+    } catch (error) {
+        snackbar.add({
+            type: 'error',
+            text: error?.response?._data?.errors?.[0]?.message || 'An error occurred'
+        });
+    } finally {
+        loading.value = false;
+    }
 };
 </script>
 
 <style lang="scss">
 .signup-form {
-  @media screen and (min-width: 769px) {
-    &_elm {
-      &-sitekey {
-        grid-row: 1;
-        grid-column: 1 / 3;
-      }
+    @media screen and (min-width: 769px) {
+        &_elm {
+            &-sitekey {
+                grid-row: 1;
+                grid-column: 1 / 3;
+            }
+        }
     }
-  }
 }
 </style>
