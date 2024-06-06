2dbd70119ad8a6fcd818230ee504228a18f1de64
VortexExpansion <gaurav@diverta.co.jp>
2024-06-06 20:35:20 +0900

    separate AppFooter to component

---
+++ components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-footer color="#1414A0" padless>
+        <v-row justify="center" no-gutters class="pt-3">
+            <a href="https://github.com/diverta/front_nuxt_auth"><small class="my-4 pt-3 l-footer_copyright">Github repository</small></a>
+            <a href="https://kuroco.app/"><img src="~/assets/images/logo-kuroco.svg" width="120" class="pl-4 l-footer_logo" /></a>
+            <v-col class="#1414A0 text-center white--text" cols="12" />
+        </v-row>
+    </v-footer>
+</template>
